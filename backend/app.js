f9999cdba2b7e283fcc180a4fe20fe50969e7d04
Muhammad Huzaifa Khan <huzaifa.khan@tpsonline.com>
2023-06-02 20:34:36 +0500

    Mongodb setup and dependencies added.

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const app = express();
+// For JSON Body Parse
+const bodyParser = require('body-parser');
+// For API Request Logging
+const morgan = require('morgan')
+// For MongoDB
+const mongoose = require('mongoose');
+
+
 // use dot env
 require('dotenv/config')
+
 // used a middleware to parse req.body in post request
-app.use(express.json());
+app.use(bodyParser.json());
+app.use(morgan("tiny"));
+
 const api = process.env.API_URL;
+const connectionString = process.env.CONNECTION_STRING;
 
 //api/v1
 // get method 
@@ -21,6 +33,15 @@ app.post(`${api}/products`, (req, res) => {
     const product = req.body;
     res.send(product);
 
+})
+
+// Connection with MongoDB
+mongoose.connect(connectionString, {
+    dbName: 'eshop-database'
+}).then(() => {
+    console.log("Database is connected")
+}).catch((err) => {
+    console.log(err);
 })
 
 app.listen(3000,()=> {

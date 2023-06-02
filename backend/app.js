5e7707fd7513b1bfa748066edda78405f6cb19c6
Muhammad Huzaifa Khan <huzaifa.khan@tpsonline.com>
2023-06-02 19:39:52 +0500

    create basic get and post API and used dotenv and express.json() middleware.

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,29 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
+// use dot env
+require('dotenv/config')
+// used a middleware to parse req.body in post request
+app.use(express.json());
+const api = process.env.API_URL;
 
-app.get('/', (req, res) => {
-    res.send("Server is ready")
-});
+//api/v1
+// get method 
+app.get(`${api}/products`, (req, res) => {
+    const product = {
+        id:1,
+        name:'Hair dresser',
+        image:'some url'
+    };
+    res.send(product);
+})
+// post method
+app.post(`${api}/products`, (req, res) => {
+    const product = req.body;
+    res.send(product);
 
-app.listen(3000, ()=> {
-    console.log(`Server is started and running on ${PORT}`);
-});
+})
+
+app.listen(3000,()=> {
+    console.log(api);
+    console.log("Server is listening")
+})

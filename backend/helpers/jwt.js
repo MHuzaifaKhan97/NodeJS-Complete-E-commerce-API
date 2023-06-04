c840b22fb057cb0f42d9ae189b314688e4305d18
Muhammad Huzaifa Khan <huzaifa.khan@tpsonline.com>
2023-06-04 16:54:00 +0500

    Excluding rest apis from authentication

--- backend/helpers/jwt.js
+++ backend/helpers/jwt.js
@@ -2,9 +2,17 @@ const { expressjwt: jwt }  = require('express-jwt');
 
 function authJwt(){
     const secret = process.env.SECRET
+    const api = process.env.API_URL
     return jwt({
         secret,
         algorithms: ['HS256']
+    }).unless({
+        path:[
+            { url: /\/api\/v1\/products(.*)/ ,methods:['GET','OPTIONS'],},
+            { url: /\/api\/v1\/categories(.*)/ ,methods:['GET','OPTIONS'],},
+            `${api}/login`,
+            `${api}/register`,
+        ]
     });
 }
 

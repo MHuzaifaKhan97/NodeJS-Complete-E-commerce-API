84d828239dfef4ff7cb7ff602e69d1274eecca0f
Muhammad Huzaifa Khan <47402181+MHuzaifaKhan97@users.noreply.github.com>
2023-06-04 15:15:10 +0500

    Register user, Hash password, get list of user, get user by id

--- backend/routers/user.js
+++ backend/routers/user.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const router = express.Router();
 // Models
-const User = require('../models/user');
+const {User} = require('../models/user');
+// Import BcryptJS
+const bcrypt = require('bcryptjs');
 
-//api/v1
-// get method 
+// Get all users
 router.get("/", async (req, res) => {
 
-    const userList = await User.find();
+    const userList = await User.find().select("-passwordHash");
      
     if(!userList){
      res.status(500).json({
@@ -16,6 +17,52 @@ router.get("/", async (req, res) => {
     }
     res.send(userList);
  })
+ // Get user minimum details
+router.get("/basicDetails", async (req, res) => {
 
+    const userList = await User.find().select("name email phone");
+     
+    if(!userList){
+     res.status(500).json({
+         success: false,
+     });
+    }
+    res.send(userList);
+ })
+//  Get single user
+ router.get("/:id", async (req, res) => {
+
+    const user = await User.findById(req.params.id).select("-passwordHash");
+    if(!user){
+     res.status(500).json({
+         success: false,
+         message:"User not found"
+     });
+    }
+    res.send(user);
+ })
+
+ // Register User
+ router. post("/", async (req,res) => {
+    let user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        passwordHash: bcrypt.hashSync(req.body.password, 10),
+        phone: req.body.phone,
+        isAdmin: req.body.isAdmin,
+        apartment: req.body.apartment,
+        street: req.body.street,
+        zip: req.body.zip,
+        city: req.body.city,
+        country: req.body.country,
+    });
+    user = await user.save();
+    if(!user){
+       return res.status(400).send({
+            message:"User can not be created"
+        })
+    }
+    res.send(user);
+ })
  
  module.exports = router;

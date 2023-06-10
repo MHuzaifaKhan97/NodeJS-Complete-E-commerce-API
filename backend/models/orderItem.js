45ddb6901a76d15d641fae1822d5115eff40edcf
Muhammad Huzaifa Khan <47402181+MHuzaifaKhan97@users.noreply.github.com>
2023-06-10 19:47:44 +0500

    Creating Order and OrderItem Model

---
+++ backend/models/orderItem.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+// Schema
+const orderItemSchema = mongoose.Schema({
+    quantity: {
+        type: Number,
+        required: true
+    },
+    product: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Product'
+    }
+})
+
+// Model
+exports.OrderItem = mongoose.model('OrderItem', orderItemSchema);
